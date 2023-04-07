f5bcfeb842cf3f5b84fba502faf7e29f991a1dd0
Adrian Petrov <forestseed@abv.bg>
2023-04-08 02:50:15 +0300

    pizza-ingredient-id functionality

--- client/src/components/Pizza/Pizza-Ingredient-Details/Pizza-Ingredient-Details.js
+++ client/src/components/Pizza/Pizza-Ingredient-Details/Pizza-Ingredient-Details.js
@@ -1,27 +1,43 @@
+import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { ApplicationContext } from "../../../contexts/ApplicationContext";
+import { pizzaServiceFactory } from "../../../services/pizzaService";
+
 export const PizzaIngredientDetails = () => {
+    const { pizzaIngredientId } = useParams();
+    const { auth } = useContext(ApplicationContext);
+    const [pizzaIngredientById, setPizzaIngredientById] = useState([]);
+    const pizzaService = pizzaServiceFactory(auth.token);
+
+    useEffect(() => {
+        pizzaService.getOnePizzaIngredientById(pizzaIngredientId).then(result => {
+            setPizzaIngredientById(result);
+        });
+    }, []);
+
     return (
         <main>
-            <section class="py-5 details" id="pizza-ingredient-details-page">
-                <div class="container">
-                    <div class="item-details">
+            <section className="py-5 details" id="pizza-ingredient-details-page">
+                <div className="container">
+                    <div className="item-details">
                         <h1 style={{fontFamily: "cursive", color: "gold", textDecoration: "underline"}}>Pizza Burger Spot</h1>
                     </div>
-                    <p class="line"></p>
-                    <div class="item-info">
-                        <p class="lead" style={{fontStyle: "italic", fontFamily: "cursive", color: "gold"}}>Pizza Ingrediet Details for :</p>
-                        <p class="lead" style={{fontSize: "18px", fontStyle: "italic", fontFamily: "cursive"}}>Type: </p>
-                        <p class="lead" style={{fontSize: "18px", fontStyle: "italic", fontFamily: "cursive"}}>Weight: </p>
+                    <p className="line"></p>
+                    <div className="item-info">
+                        <p className="lead" style={{fontStyle: "italic", fontFamily: "cursive", color: "gold"}}>Pizza Ingrediet Details for {pizzaIngredientById.name}:</p>
+                        <p className="lead" style={{fontSize: "18px", fontStyle: "italic", fontFamily: "cursive"}}>Type: {pizzaIngredientById.type}</p>
+                        <p className="lead" style={{fontSize: "18px", fontStyle: "italic", fontFamily: "cursive"}}>Weight: {pizzaIngredientById.weight}g</p>
                     </div>
-                    <p class="line"></p>
-                    <div class="item-picanddesc">
+                    <p className="line"></p>
+                    <div className="item-picanddesc">
                         <div>
-                            <img class="img-fluid rounded"
-                                src=""
+                            <img className="img-fluid rounded"
+                                src={pizzaIngredientById.picture}
                                 alt="pizza-ingredient-image"/>
                         </div>
-                        <div class="item-desc">
+                        <div className="item-desc">
                             <h5 style={{fontStyle: "italic", fontFamily: "cursive"}}>Description</h5>
-                            <textarea class="lead" disabled style={{fontStyle: "italic", fontFamily: "cursive"}}></textarea>
+                            <textarea className="lead" disabled style={{fontStyle: "italic", fontFamily: "cursive"}} value={pizzaIngredientById.description}></textarea>
                         </div>
                     </div>
                 </div>
