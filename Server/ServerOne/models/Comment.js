3c26aa9d243067964560e5eb317e5511c0696ee3
Adrian Petrov <forestseed@abv.bg>
2023-04-15 21:50:58 +0300

    server comments added

---
+++ Server/ServerOne/models/Comment.js
@@ -0,0 +1,17 @@
+const {Schema, model, Types} = require('mongoose');
+
+const commentSchema = new Schema({
+    comment:{
+        type: String,
+        required: true,
+        minlength: [3, 'The comment must be at least 3 characters long!']
+    }, 
+    owner:{
+        type: Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+},{ timestamps: { createdAt: 'created_at'}});
+
+const Comment = model('Comment', commentSchema);
+module.exports = Comment;
---
+++ Server/ServerOne/models/Restaurant.js
@@ -0,0 +1,39 @@
+const {Schema, model, Types} = require('mongoose');
+
+const restaurantSchema = new Schema({
+    name:{
+        type: String,
+        required: true,
+        minlength: [3, 'The name must be at least 3 characters long!']
+    },
+    address:{
+        type: String,
+        required: true,
+        minlength: [3, 'The address must be at least 3 characters long!']
+    },
+    description:{
+        type: String,
+        required: true,
+        minlength: [3, 'The description must be at least 3 characters long!']
+    },
+    rating:{
+        type: Number,
+        required: true,
+        default: 0,
+        min: [0, 'Rating cannot be negative!']
+    },
+    owner:{
+        type: Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    comments:{
+        type: [Types.ObjectId],
+        default: [],
+        ref: 'Comment',
+        required: true
+    }
+},{ timestamps: { createdAt: 'created_at'}});
+
+const Restaurant = model('Restaurant', restaurantSchema);
+module.exports = Restaurant;
