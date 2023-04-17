c700ae7095abc27ede0a81aae85673e5672ab21c
Adrian Petrov <forestseed@abv.bg>
2023-04-17 12:26:35 +0300

    doc

--- client/src/components/Core/Footer/Footer.js
+++ client/src/components/Core/Footer/Footer.js
@@ -5,11 +5,20 @@ export const Footer = () => {
         <footer id="sticky-footer" className="bg-dark">
             <div className="container">
                 <p className="m-0 text-center text-white" style={{fontSize: "18px", fontFamily: "cursive"}}> &copy; All rights reserved.  
+                    &nbsp;
                     <Link to="/"> Home </Link>
-                    |
+                    &nbsp;
                     <Link to="/about"> About </Link>
-                    |
+                    &nbsp;
                     <Link to="/contacts"> Contacts </Link>
+                    &nbsp;
+                    <Link to='https://twitter.com'><i class="fab fa-twitter"></i></Link>
+                    &nbsp;
+                    <Link to='https://www.facebook.com'><i class="fab fa-facebook-square"></i></Link>
+                    &nbsp;
+                    <Link to='https://www.instagram.com/'><i class="fab fa-instagram"></i></Link>
+                    &nbsp;
+                    <Link to='https://www.youtube.com'><i class="fab fa-youtube"></i></Link>
                 </p>
             </div>
         </footer>
