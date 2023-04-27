8eed86b4e17b58e4224d44aa0d6bb54f06dc352f
Adrian Petrov <forestseed@abv.bg>
2023-04-28 01:59:47 +0300

    pizza-ingredients page

--- client/src/components/Pizza/Pizza-Ingredient/Pizza-Ingredients.js
+++ client/src/components/Pizza/Pizza-Ingredient/Pizza-Ingredients.js
@@ -22,32 +22,35 @@ export const PizzaIngredients = () => {
     }, []);
 
     return (
-        <main>
-            <section className="container" id="pizza-ingredient">
-                <header className="ingredient">
-                    <h1 style={{fontFamily: "cursive", color: "gold", textDecoration: "underline"}}>Pizza & Burger Spot</h1>
-                    <p className="lead" style={{fontStyle: "italic", fontFamily: "cursive"}}>We make our pizzas with the freshest ingredients out there!</p>
-                    <p> </p>
-                    <p className="line"></p>
-                    <p>&nbsp;&nbsp;&nbsp;</p>
-                    <p className="lead" style={{fontStyle: "italic", fontFamily: "cursive"}}>Here is a list of products we use in the making of the pizzas!</p>
-                    <p> </p>
-                    <p className="line"></p>
-                </header>
-                <div className="row text-center">
-                    {
-                        pizzaIngredients.length > 0 && pizzaIngredients.map(x => <PizzaIngredient key={x._id} {...x}/>)
-                    }
-                    {
-                        (pizzaIngredients.length <= 0 || errorFetchingPizzaIngredientsData) && (
-                            <div className="no-ingredients">
-                                <img src="/assets/pictures/main/404missin.png" alt="missing"/>
-                                <p className="lead">Please try again later...</p>
-                            </div>
-                        )
-                    }
-                </div>
-            </section>
-        </main>
+        <>
+            <main>
+                <section className="container" id="pizza-ingredient">
+                    <header className="ingredient">
+                        <h1 style={{fontFamily: "cursive", color: "gold", textDecoration: "underline"}}>Pizza & Burger Spot</h1>
+                        <p className="lead" style={{fontStyle: "italic", fontFamily: "cursive"}}>We make our pizzas with the freshest ingredients out there!</p>
+                        <p> </p>
+                        <p className="line"></p>
+                        <p>&nbsp;&nbsp;&nbsp;</p>
+                        <p className="lead" style={{fontStyle: "italic", fontFamily: "cursive"}}>Here is a list of products we use in the making of the pizzas!</p>
+                        <p> </p>
+                        <p className="line"></p>
+                    </header>
+                    <div className="row text-center">
+                        {
+                            pizzaIngredients.length > 0 && pizzaIngredients.map(x => <PizzaIngredient key={x._id} {...x}/>)
+                        }
+                        {
+                            (pizzaIngredients.length <= 0 || errorFetchingPizzaIngredientsData) && (
+                                <div className="no-ingredients">
+                                    <img src="/assets/pictures/main/404missin.png" alt="missing"/>
+                                    <p className="lead">Please try again later...</p>
+                                </div>
+                            )
+                        }
+                    </div>
+                </section>
+            </main>
+            <div className="footer-divider"></div>
+        </>
     );
 };
