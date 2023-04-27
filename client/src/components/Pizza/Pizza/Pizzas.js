e5b2b95596ef6f883a7260d82718e99cfa4cd4d9
Adrian Petrov <forestseed@abv.bg>
2023-04-28 01:56:05 +0300

    pizzas page

--- client/src/components/Pizza/Pizza/Pizzas.js
+++ client/src/components/Pizza/Pizza/Pizzas.js
@@ -11,41 +11,44 @@ export const Pizzas = () => {
     const { pizzas, errorFetchingPizzasData } = useContext(PizzaContext)
 
     return (
-        <main>
-            <section className="container" id="pizzas-main">
-                <header className="ingredient">
-                    <h1 style={{fontFamily: "cursive"}}>
-                        <p style={{fontSize: "36px", fontFamily: "cursive", color: "gold", textDecoration: "underline"}}>Pizza & Burger Spot</p>
-                        <p className="lead" style={{fontStyle: "italic", fontFamily: "cursive"}}>We make the best Pizzas in town and here is a list of our Pizzas!</p>
-                        <p> </p>
-                        <p className="line"></p>
-                        <p>&nbsp;&nbsp;&nbsp;</p>
+        <>
+            <main>
+                <section className="container" id="pizzas-main">
+                    <header className="ingredient">
+                        <h1 style={{fontFamily: "cursive"}}>
+                            <p style={{fontSize: "36px", fontFamily: "cursive", color: "gold", textDecoration: "underline"}}>Pizza & Burger Spot</p>
+                            <p className="lead" style={{fontStyle: "italic", fontFamily: "cursive"}}>We make the best Pizzas in town and here is a list of our Pizzas!</p>
+                            <p> </p>
+                            <p className="line"></p>
+                            <p>&nbsp;&nbsp;&nbsp;</p>
+                            {
+                                isAuthenticated && (
+                                    <>
+                                        <Link to="/pizzas/create" style={{color: "gold"}}> Add Pizza </Link> 
+                                        | 
+                                    </>
+                                )
+                            }
+                            <Link to="/pizzas/pizza-ingredients" style={{color: "gold"}}> Ingredients </Link>
+                        </h1>
+                        <p className="lead" style={{fontStyle: "italic", fontFamily: "cursive", fontSize: "22px"}}>Pizzas List:</p>
+                    </header>
+                    <div className="row text-center">
                         {
-                            isAuthenticated && (
-                                <>
-                                    <Link to="/pizzas/create" style={{color: "gold"}}> Add Pizza </Link> 
-                                    | 
-                                </>
+                            pizzas.length > 0 && pizzas.map(x => <Pizza key={x._id} {...x}/>)
+                        }
+                        {
+                            (pizzas.length <= 0 || errorFetchingPizzasData) && (
+                                <div className="no-ingredients">
+                                    <img src="/assets/pictures/main/404missin.png" alt="missing"/>
+                                    <p className="lead">Please try again later...</p>
+                                </div>
                             )
                         }
-                        <Link to="/pizzas/pizza-ingredients" style={{color: "gold"}}> Ingredients </Link>
-                    </h1>
-                    <p className="lead" style={{fontStyle: "italic", fontFamily: "cursive", fontSize: "22px"}}>Pizzas List:</p>
-                </header>
-                <div className="row text-center">
-                    {
-                        pizzas.length > 0 && pizzas.map(x => <Pizza key={x._id} {...x}/>)
-                    }
-                    {
-                        (pizzas.length <= 0 || errorFetchingPizzasData) && (
-                            <div className="no-ingredients">
-                                <img src="/assets/pictures/main/404missin.png" alt="missing"/>
-                                <p className="lead">Please try again later...</p>
-                            </div>
-                        )
-                    }
-                </div>
-            </section>
-        </main>
+                    </div>
+                </section>
+            </main>
+            <div className="footer-divider"></div>
+        </>
     );
 };
