4ae6d12ee5018d36ca0ff4afecd53825d943e5dd
Adrian Petrov <forestseed@abv.bg>
2023-04-27 19:59:47 +0300

    footer fix

--- client/src/components/Core/Footer/Footer.js
+++ client/src/components/Core/Footer/Footer.js
@@ -6,8 +6,14 @@ export const Footer = () => {
     return (
         <footer id="footer" className="footer">
             <div className="footer-container">
-                <div className="copyright">
-                    <p className="copyright-p"><span className="copyright-span">&copy;</span> All rights reserved</p>
+                <div className="socials-links-container">
+                    <ul className="socials-links">
+                        <li className="socials-link-li"><Link className="socials-link socials-link-twitter" to='https://twitter.com' target="_blank"><i className="fab fa-twitter"></i></Link></li>
+                        <li className="socials-link-li"><Link className="socials-link socials-link-facebook" to='https://www.facebook.com' target="_blank"><i className="fab fa-facebook-square"></i></Link></li>
+                        <li className="socials-link-li"><Link className="socials-link socials-link-instagram" to='https://www.instagram.com/' target="_blank"><i className="fab fa-instagram"></i></Link></li>
+                        <li className="socials-link-li"><Link className="socials-link socials-link-youtube" to='https://www.youtube.com' target="_blank"><i className="fab fa-youtube"></i></Link></li>
+                        <li className="socials-link-li"><Link className="socials-link socials-link-linkedin" to='https://www.linkedin.com' target="_blank"><i className="fab fa-linkedin"></i></Link></li>
+                    </ul>
                 </div>
                 <div className="page-links-container">
                     <ul className="page-links">
@@ -17,14 +23,8 @@ export const Footer = () => {
                         <li className="page-link-li"><Link className="page-link" to="https://github.com/adriqnn/ReactProject/tree/master/client" target="_blank">Documentation</Link></li>
                     </ul>
                 </div>
-                <div className="socials-links-container">
-                    <ul className="socials-links">
-                        <li className="socials-link-li"><Link className="socials-link socials-link-twitter" to='https://twitter.com' target="_blank"><i className="fab fa-twitter"></i></Link></li>
-                        <li className="socials-link-li"><Link className="socials-link socials-link-facebook" to='https://www.facebook.com' target="_blank"><i className="fab fa-facebook-square"></i></Link></li>
-                        <li className="socials-link-li"><Link className="socials-link socials-link-instagram" to='https://www.instagram.com/' target="_blank"><i className="fab fa-instagram"></i></Link></li>
-                        <li className="socials-link-li"><Link className="socials-link socials-link-youtube" to='https://www.youtube.com' target="_blank"><i className="fab fa-youtube"></i></Link></li>
-                        <li className="socials-link-li"><Link className="socials-link socials-link-linkedin" to='https://www.linkedin.com' target="_blank"><i className="fab fa-linkedin"></i></Link></li>
-                    </ul>
+                <div className="copyright">
+                    <p className="copyright-p"><span className="copyright-span">&copy;</span> All rights reserved</p>
                 </div>
             </div>
         </footer>
