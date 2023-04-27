371e769d09c056625d4f3e5873afd1c1428ecd37
Adrian Petrov <forestseed@abv.bg>
2023-04-28 01:56:58 +0300

    burgers page

--- client/src/components/Burger/Burger/Burgers.js
+++ client/src/components/Burger/Burger/Burgers.js
@@ -11,41 +11,44 @@ export const Burgers = () => {
     const { burgers, errorFetchingBurgersData } = useContext(BurgerContext);
  
     return (
-        <main>
-            <section className="container" id="burgers-main">
-                <header className="ingredient">
-                    <h1 style={{fontFamily: "cursive"}}>
-                        <p style={{fontSize: "36px", fontFamily: "cursive", color: "gold", textDecoration: "underline"}}>Pizza & Burger Spot</p>
-                        <p className="lead" style={{fontStyle: "italic", fontFamily: "cursive"}}>We make the best Burgers in town and here is a list of our Burgers!</p>
-                        <p> </p>
-                        <p className="line"></p>
-                        <p>&nbsp;&nbsp;&nbsp;</p>
-                        { 
-                            isAuthenticated && (
-                                <>
-                                    <Link to="/burgers/create" style={{color: "gold"}}> Add Burger </Link>
-                                    | 
-                                </>
+        <>
+            <main>
+                <section className="container" id="burgers-main">
+                    <header className="ingredient">
+                        <h1 style={{fontFamily: "cursive"}}>
+                            <p style={{fontSize: "36px", fontFamily: "cursive", color: "gold", textDecoration: "underline"}}>Pizza & Burger Spot</p>
+                            <p className="lead" style={{fontStyle: "italic", fontFamily: "cursive"}}>We make the best Burgers in town and here is a list of our Burgers!</p>
+                            <p> </p>
+                            <p className="line"></p>
+                            <p>&nbsp;&nbsp;&nbsp;</p>
+                            { 
+                                isAuthenticated && (
+                                    <>
+                                        <Link to="/burgers/create" style={{color: "gold"}}> Add Burger </Link>
+                                        | 
+                                    </>
+                                )
+                            }
+                            <Link to="/burgers/burger-ingredients" style={{color: "gold"}}> Ingredients </Link>
+                        </h1>
+                        <p className="lead" style={{fontStyle: "italic", fontFamily: "cursive", fontSize: "22px"}}>Burgers List:</p>
+                    </header>
+                    <div className="row text-center">
+                        {
+                            burgers.length > 0 && burgers.map(x => <Burger key={x._id} {...x}/>)
+                        }
+                        {
+                            (burgers.length <= 0 || errorFetchingBurgersData) && (
+                                <div className="no-ingredients">
+                                    <img src="/assets/pictures/main/404missin.png" alt="missing"/>
+                                    <p className="lead">Please try again later...</p>
+                                </div>
                             )
                         }
-                        <Link to="/burgers/burger-ingredients" style={{color: "gold"}}> Ingredients </Link>
-                    </h1>
-                    <p className="lead" style={{fontStyle: "italic", fontFamily: "cursive", fontSize: "22px"}}>Burgers List:</p>
-                </header>
-                <div className="row text-center">
-                    {
-                        burgers.length > 0 && burgers.map(x => <Burger key={x._id} {...x}/>)
-                    }
-                    {
-                        (burgers.length <= 0 || errorFetchingBurgersData) && (
-                            <div className="no-ingredients">
-                                <img src="/assets/pictures/main/404missin.png" alt="missing"/>
-                                <p className="lead">Please try again later...</p>
-                            </div>
-                        )
-                    }
-                </div>
-            </section>
-        </main>        
+                    </div>
+                </section>
+            </main>    
+            <div className="footer-divider"></div>    
+        </>
     );
 };
